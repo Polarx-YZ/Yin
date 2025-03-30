c6c7b14c4e0e2472e5c748dd6a1e2cc879250871
Rin <Notpolarpup@gmail.com>
2025-03-30 17:46:06 -0500

    added cogs

---
+++ cogs/ping.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot is ready!")
+        
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("Pong!")
+        
+        
+async def setup(bot):
+    await bot.add_cog(ping(bot))
