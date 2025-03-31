c4750496b0bbc3e37d4308c8dd6d327a4ca609a1
Rin <Notpolarpup@gmail.com>
2025-03-30 22:13:25 -0500

    made it easier to edit values

--- cogs/commands/help.py
+++ cogs/commands/help.py
@@ -3,19 +3,24 @@ from discord.ext import commands
 import settings
 settings.init()
 
+
 class help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-      
+
     @commands.command()
     async def help(self, ctx):
-        
+
+        devs = ', '.join(settings.config.get('devs'))
+        support_server = settings.config.get("supportServer")
+
         helpEmbed = discord.Embed(
-            title=f"{settings.config.get('botName')} | Help", 
-            description=f"A bot made by people who have no idea what they are doing. \nJoin the [Support Server!](https://discord.gg/xHB5XUMhbu)",
-            )
-        helpEmbed.set_footer(text= f"Made by {', '.join(settings.config.get('devs'))}")
+            title=f"{settings.config.get('botName')} | Help",
+            description=f"A bot made by people who have no idea what they are doing. \nJoin the [Support Server!]({support_server})",
+        )
+        helpEmbed.set_footer(text=f"Made by {devs}")
         await ctx.reply(embed=helpEmbed)
-        
+
+
 async def setup(bot):
     await bot.add_cog(help(bot))
