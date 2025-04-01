bc589b4bd1196be5fac2d750b569a068411e9a47
Rin <Notpolarpup@gmail.com>
2025-03-31 19:30:16 -0500

    started music cog

---
+++ cogs/commands/music.py
@@ -0,0 +1,73 @@
+import discord
+from discord.ext import commands
+
+
+class music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Plays a song
+    @commands.command()
+    async def play(self, ctx):
+        await ctx.reply("Command not yet implemented")
+        
+        # If there is no song playing and the queue is empty then play the song provided
+        
+        # If there is a song already playing then add the song to the end of the queue
+        
+        # If no song is provided then let the user know
+        
+        # If the queue is NOT empty and is paused/stopped then resume playing the queue
+
+    @commands.command()
+    async def stop(self, ctx):
+        await ctx.reply("Command not yet implemented")
+        
+        # Stop playing the song and keep all songs in the queue
+        
+
+    @commands.command()
+    async def queue(self, ctx):
+        await ctx.reply("Command not yet implemented")
+        
+        # Show the next 10 songs in the queue with the song name and artist
+        
+        # Send this in an embed please
+
+    @commands.command()
+    async def join(self, ctx):
+        await ctx.reply("Command not yet implemented")
+        
+        # Make the bot join the voice channel the invoker is in.
+        
+        # If the invoker is not in a voice channel then let the invoker know
+
+    @commands.command()
+    async def leave(self, ctx):
+        await ctx.reply("Command not yet implemented")
+        
+        # Leave the voice channel but do NOT clear the queue
+
+    @commands.command()
+    async def add(self, ctx):
+        await ctx.reply("Command not yet implemented")
+        
+        # add a song to the queue
+        
+    @commands.command()
+    async def skip(self, ctx):
+        await ctx.reply("Command not yet implemented")
+        
+        # Skip the song that is currently playing
+        
+        # If no song is playing then let the invoker know
+        
+    @commands.command()
+    async def clear(self, ctx):
+        await ctx.reply("Command not yet implemented")
+        
+        # Clear the queue
+
+
+async def setup(bot):
+    await bot.add_cog(music(bot))
