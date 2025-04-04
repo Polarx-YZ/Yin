97f8d90a1957c9977d891a387fbd1c5bbd16313f
Rin <Notpolarpup@gmail.com>
2025-04-03 19:22:03 -0500

    dad bot

--- cogs/events/message.py
+++ cogs/events/message.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 from settings import config
-import re
 
 
 class Message(commands.Cog):
@@ -13,17 +12,35 @@ class Message(commands.Cog):
         if ctx.author.bot:
             return
 
+        user = ctx.author
         dad_triggers = ["i'm", "am", "im"]
 
         # Checks if any dad_triggers are in the message and stores the string that triggered it
-        # TODO Fix it so that it starts the message after the "I am" It will start at a word earlier if it has "am" or "im" in it like "imagine"
-        dad_trigger = next((trigger for trigger in dad_triggers if any(x in trigger for x in ctx.content.split())), None)
+        if (any(x for x in dad_triggers if x in ctx.content.lower())):
+            for trigger in dad_triggers:
+                for index, word in enumerate(ctx.content.split(" ")):
+                    if word == trigger:
+                        words_after = ctx.content.split()[index + 1:]
+                        return await ctx.reply(f"Hi {" ".join(words_after)}! I'm Dad!")
+                    
+        # Respond to 'shouting'
+        if (ctx.content.isupper()):
+            return await ctx.reply(f"Hey {user.mention}, no yelling!")
+        
+        #! OLD CODE DOESN'T WORK AND IT'S WAY TOO COMPLICATED LOL - @Polarx-YZ
+        # dad_trigger = next((trigger for trigger in dad_triggers if any(x in trigger for x in ctx.content.split())), None)
 
-        if dad_trigger is not None:
-            before, key, after = ctx.content.partition(dad_trigger)
-            if key == "":
-                return
-            await ctx.reply(f"Hi{after}! I'm Dad")
+        # if dad_trigger is not None:
+        #     print(dad_trigger)
+        #     after = re.search(r"(?<={dad_trigger})\s*(.*)", ctx.content)
+            
+        #     print(after)
+        
+        #     #await ctx.reply(f"Hi {after}! I'm Dad!")
+        #     # before, key, after = ctx.content.partition(dad_trigger)
+        #     # if key == "":
+        #     #     return
+        #     # await ctx.reply(f"Hi{after}! I'm Dad")
 
 
 async def setup(bot):
