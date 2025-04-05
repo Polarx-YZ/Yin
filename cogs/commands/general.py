2779861e8cbf7bfc899efb0218ddc069c778253b
Rin <Notpolarpup@gmail.com>
2025-04-04 19:47:55 -0500

    added uptime

--- cogs/commands/general.py
+++ cogs/commands/general.py
@@ -7,11 +7,11 @@ from time import time
 class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.start_time = time()
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        global start_time
-        start_time = time()
+    # @commands.Cog.listener()
+    # async def on_ready(self):
+    #     self.start_time = time()
     
     @commands.command()
     async def ping(self, ctx):
@@ -33,17 +33,17 @@ class General(commands.Cog):
         total_commands = len(self.bot.commands)
         total_servers = len(self.bot.guilds)
         total_members = 0
-        uptime = str(datetime.timedelta(seconds=int(round(time()-start_time))))
+        uptime = str(datetime.timedelta(seconds=int(round(time()-self.start_time))))
         devs = ", ".join(config.get("devs"))
         
-        #* In the future I would like this to not include duplicate members
+        # TODO In the future I would like this to not include duplicate members
         for guild in self.bot.guilds:
             user_count = len([x for x in guild.members if not x.bot]) # Excludes bots from the member count
             total_members += user_count
 
         embed = discord.Embed(
-            title=f"- Bot Stats -",
-            description=f"Stats for {config.get("botName")}"
+            title=f"- Bot Info -",
+            description=f"Info for {config.get("botName")}"
         )
         embed.add_field(name="Total Commands", value=total_commands)
         embed.add_field(name="Total Servers", value=total_servers)
