011fabfcd244b517e318e5896a00a599b2e38ea4
SpaceDuck16 <spaceduckplayz@gmail.com>
2025-04-05 16:05:55 -0500

    Added add, subtract, multiply, divide and round commands
    Co-authored-by: Rin <Polarx-YZ@users.noreply.github.com>

--- cogs/commands/calculator.py
+++ cogs/commands/calculator.py
@@ -1,5 +1,5 @@
 '''
-For @MudaiP
+For @SpaceDuck16
 '''
 
 import discord
@@ -10,21 +10,50 @@ class Calculator(commands.Cog):
         self.bot = bot
       
     @commands.command()
-    async def add(self, ctx):
-        pass
-    
-    @commands.command()
-    async def subtract(self, ctx):
-        pass
-    
-    @commands.command()
-    async def multiply(self, ctx):
-        pass
-    
-    @commands.command()
-    async def divide(self, ctx):
-        pass
+    async def add(self, ctx, *args: int):
         
+        sum = args[0]
+        
+        for num in args[1:]:
+            sum += num
+        
+        await ctx.reply(str(sum))
+        
+    @commands.command()
+    async def subtract(self, ctx, *args: int):
+        
+        diff = args[0]
+
+        for num in args[1:]:
+            diff -= num
+
+        await ctx.reply(str(diff))
+    
+    @commands.command()
+    async def multiply(self, ctx, *args: int):
+        
+        prod = args[0]
+        
+        for num in args[1:]:
+            prod *= num
+
+        await ctx.reply(str(prod))
+    
+    @commands.command()
+    async def divide(self, ctx, *args: int):
+        
+        quot = args[0]
+
+        for num in args[1:]:
+            quot /= num
+
+        await ctx.reply(str(round(quot, 2)))
+        
+    @commands.command()
+    async def round(self, ctx, num: float, place: int=1):
+        await ctx.reply(str(round(num, place)))
+    
+    
     @commands.command()
     async def calculate(self, ctx):
         pass
