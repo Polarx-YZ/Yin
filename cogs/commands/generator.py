e81c87906a3891a005124dd7801743a1f0f0b20a
Rin <Notpolarpup@gmail.com>
2025-04-04 19:48:18 -0500

    fixed

--- cogs/commands/generator.py
+++ cogs/commands/generator.py
@@ -3,15 +3,26 @@ from discord.ext import commands
 import re
 
 
+
 class Generator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def get_vowels(self, sentence):
+        vowels = list("aieou")
+        letters = []
+        for letter in sentence:
+            if letter in vowels:
+                letters.append(letter)
+        return letters
+
     @commands.command(brief="Talk like LOR3!", description="Talk like LOR3 from the Gaming Tavern!")
     async def lore(self, ctx, *args):
-        message = re.sub(r"(?is)e", "3", " ".join(args)
-                         )  # Replace the E's with 3's
-        message = re.sub(r"(?is)s", "2", message)  # Replace the S's with 2's
+        if len(args) <= 0:
+            return await ctx.reply("Please supply a message to generate!")
+        
+        message = re.sub(r"(?is)e", "3", " ".join(args)) 
+        message = re.sub(r"(?is)s", "2", message)
         message = re.sub(r"(?is)b", "6", message)
         message = re.sub(r"(?is)t", "7", message)
         message = re.sub(r"(?is)k", "l<", message)
@@ -28,6 +39,9 @@ class Generator(commands.Cog):
 
     @commands.command(aliases=["meow", "nyan"], brief="Meow meow meow!", description="Talk like a cat!")
     async def cat(self, ctx, *args):
+        if len(args) <= 0:
+            return await ctx.reply("Please supply a message to generate nya!")
+        
         meows = {
             "a": "maow",
             "e": "meow",
@@ -36,17 +50,9 @@ class Generator(commands.Cog):
             "u": "mew"
         }
 
-        def get_vowels(sentence):
-            vowels = list("aieou")
-            letters = []
-            for letter in sentence:
-                if letter in vowels:
-                    letters.append(letter)
-            return letters
-
         message = ""
-        vowels = get_vowels("".join(args))
-
+        vowels = await self.get_vowels("".join(args))
+            
         for vowel in vowels:
             message += " " + meows.get(vowel, "meow")
 
@@ -54,7 +60,10 @@ class Generator(commands.Cog):
 
     @commands.command(aliases=["woof", "wan", "bark"], brief="Woof woof woof!!", description="Talk like a dog!")
     async def dog(self, ctx, *args):
-        meows = {
+        if len(args) <= 0:
+            return await ctx.reply("Please supply a message to generate woof!")
+        
+        barks = {
             "a": "wan",
             "e": "grer",
             "i": "*whine*",
@@ -62,19 +71,11 @@ class Generator(commands.Cog):
             "u": "wuuu"
         }
 
-        def get_vowels(sentence):
-            vowels = list("aieou")
-            letters = []
-            for letter in sentence:
-                if letter in vowels:
-                    letters.append(letter)
-            return letters
-
         message = ""
-        vowels = get_vowels("".join(args))
+        vowels = await self.get_vowels("".join(args))
 
         for vowel in vowels:
-            message += " " + meows.get(vowel, "woof")
+            message += " " + barks.get(vowel, "woof")
 
         await ctx.reply(message)
 
