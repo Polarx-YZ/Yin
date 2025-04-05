c5eebefbaf0233fa9375752c65c529213c3ca29f
SpaceDuck16 <spaceduckplayz@gmail.com>
2025-04-05 17:16:04 -0500

    - Added calculator help - Calculator outputs equations - /0 message
    Co-authored-by: Rin <Polarx-YZ@users.noreply.github.com>

--- cogs/commands/calculator.py
+++ cogs/commands/calculator.py
@@ -9,7 +9,7 @@ class Calculator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
       
-    @commands.command(aliases=["sum"], brief="Add numbers", description="Add numbers together", usage="`Num1` `Num2` `etc...`")
+    @commands.command(aliases=["sum", "plus", "total"], brief="Add numbers", description="Outputs a sum of two or more numbers", usage="`Num1` `Num2` `etc...`")
     async def add(self, ctx, *args: float):
         
         sum = args[0]
@@ -17,19 +17,23 @@ class Calculator(commands.Cog):
         for num in args[1:]:
             sum += num
         
-        await ctx.reply(str(sum))
+        addends = " + ".join(str(x) for x in args)
+
+        await ctx.reply(addends + " = " + str(sum)) 
         
-    @commands.command()
+    @commands.command(aliases=["diff", "difference", "minus", "sub"], brief="Subtract numbers", description="Outputs a difference between two or more numbers", usage="`Num1` `Num2` `etc...`")
     async def subtract(self, ctx, *args: float):
         
         diff = args[0]
 
         for num in args[1:]:
             diff -= num
+            
+        equation = " - ".join(str(x) for x in args)
 
-        await ctx.reply(str(diff))
+        await ctx.reply(equation + " = " + str(diff))
     
-    @commands.command()
+    @commands.command(aliases=["prod", "product", "times", "mult"], brief="Multiply numbers", description="Outputs a product of two or more numbers", usage="`Num1` `Num2` `etc...`")
     async def multiply(self, ctx, *args: float):
         
         prod = args[0]
@@ -37,17 +41,26 @@ class Calculator(commands.Cog):
         for num in args[1:]:
             prod *= num
 
-        await ctx.reply(str(prod))
+        factors = " x ".join(str(x) for x in args)
+
+        await ctx.reply(factors + " = " + str(prod))
     
-    @commands.command()
+    @commands.command(aliases=["quot", "quotient", "div"], brief="Divide numbers", description="Outputs a quotient (rounded to hundredths) of two or more numbers", usage="`Num1` `Num2` `etc...`")
     async def divide(self, ctx, *args: float):
+
+        for num in args[1:]:
+            if num == 0:
+                await ctx.reply("You can't divide by zero you dumbo!")
+                return
         
         quot = args[0]
 
         for num in args[1:]:
             quot /= num
 
-        await ctx.reply(str(round(quot, 2)))
+        dividends = " / ".join(str(x) for x in args)
+
+        await ctx.reply(dividends + " = " + str(round(quot, 2)))
         
     @commands.command()
     async def round(self, ctx, num: float, place: int=1):
