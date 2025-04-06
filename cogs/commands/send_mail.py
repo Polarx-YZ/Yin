692c72fb94f752ec37b3490d9f789d8963dc8ede
FitYoshiBoy23 <awadood1116@gmail.com>
2025-04-05 21:57:19 -0400

    Yin bot has my gmail password, so it will send emails

---
+++ cogs/commands/send_mail.py
@@ -0,0 +1,54 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+import os
+
+import discord
+from discord.ext import commands
+
+
+load_dotenv(override=True)
+
+
+class SendMail(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def sendmail(self, ctx, *, message: str):
+
+        await ctx.reply("`Sending email...`")
+        self.send_email(message)
+        await ctx.reply("`Email sent!`")
+
+    def send_email(self, msg):
+
+        sender_email = "pythonsmtpdeveloper@gmail.com"
+        receiver_email = [
+            "polarkun761@gmail.com",
+            "notpolarpup@gmail.com"
+        ]
+        password = os.getenv("GMAIL_PASSWORD")
+
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Test Yin Bot Email Send"
+        message["From"] = sender_email
+        message["To"] = ", ".join(receiver_email)
+
+        # Create the plain-text
+        part1 = MIMEText(msg, "plain")
+
+        # Add plain text part to MIMEMultipart message
+        message.attach(part1)
+
+        # Create secure connection with server and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+            
+            
+async def setup(bot):
+    await bot.add_cog(SendMail(bot))
