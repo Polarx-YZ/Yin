84e5c05db8e6cac6bda2268b0915ef664f185463
Rin <Notpolarpup@gmail.com>
2025-04-18 18:22:55 -0500

    rewrote with easy-pil

--- cogs/commands/image_generation.py
+++ cogs/commands/image_generation.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-import PIL.Image as Image
+from easy_pil import Editor, load_image_async
 from io import BytesIO
 import requests
 
@@ -11,45 +11,36 @@ class ImageGeneration(commands.Cog):
         
     @commands.command()
     async def slap(self, ctx, victim: discord.Member):
-        slap_template = Image.open("images/slap.jpg")
+        editor = Editor("images/slap.jpg")
         
-        pfp = await self.open_pfp(ctx.author.avatar.url) 
-        pfp2 = await self.open_pfp(victim.avatar.url)
+        pfp_owner = ctx.author.avatar.url
+        victim_pfp_owner = victim.avatar.url
+
+        if ctx.author.id == victim.id:
+            pfp_owner = self.bot.user.avatar.url
         
-        #slap_template = slap_template.resize((405 * 2, 612 * 2))
+        pfp = await load_image_async(pfp_owner) 
+        pfp2 = await load_image_async(victim_pfp_owner)
         
         # Place the invoker's pfp
         pfp = pfp.resize((150, 150))
-        mask = Image.new('L', pfp.size, 255)
         pfp = pfp.rotate(15, expand=True)
-        mask = mask.rotate(15, expand=True)
-        slap_template.paste(pfp, (-15, 65), mask)
+        editor.paste(pfp, (-15, 65))
         
         # Place the victim's pfp
         pfp2 = pfp2.resize((220, 220))
-        mask = Image.new('L', pfp2.size, 255)
         pfp2 = pfp2.rotate(-35, expand=1)
-        mask = mask.rotate(-35, expand=True)
-        slap_template.paste(pfp2, (350, 10), mask)
-        
+        editor.paste(pfp2, (350, 10))
+
         embed = discord.Embed(
             description = f"### {ctx.author.mention} slapped {victim.mention}!!",
             color = discord.Color.red()
         )
         
+        file = discord.File(fp=editor.image_bytes, filename="file.png")
         
-        with BytesIO() as image_binary:
-            slap_template.save(image_binary, "PNG")
-            image_binary.seek(0)
-            file = discord.File(fp=image_binary, filename="file.png")
-            
-            embed.set_image(url="attachment://file.png")
-            await ctx.reply(file=file, embed=embed)
-    
-    async def open_pfp(self, asset):
-        data = BytesIO(requests.get(asset).content)
-        pfp = Image.open(data)
-        return pfp
+        embed.set_image(url="attachment://file.png")
+        await ctx.reply(file=file, embed=embed)
     
 async def setup(bot):
     await bot.add_cog(ImageGeneration(bot))
