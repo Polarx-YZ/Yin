95c9bdaeb06ec92aa1169f3e608f3804fec952c8
Rin <Notpolarpup@gmail.com>
2025-04-18 18:33:57 -0500

    renamed

--- cogs/commands/social.py
+++ cogs/commands/social.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 from easy_pil import Editor, load_image_async
 
-class ImageGeneration(commands.Cog):
+class Social(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
@@ -41,4 +41,4 @@ class ImageGeneration(commands.Cog):
         await ctx.reply(file=file, embed=embed)
     
 async def setup(bot):
-    await bot.add_cog(ImageGeneration(bot))
+    await bot.add_cog(Social(bot))
